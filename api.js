ceadff87636cd5fe439053ff6ab8f980b3e841ef
DosImpact <ypd03008@gmail.com>
2020-02-10 17:41:07 +0900

    update axios

--- api.js
+++ api.js
@@ -1,34 +1,38 @@
 import axios from "axios";
 
+const params = {
+  api_key: "59caaaf6db048b723bf9adf4f0380be1",
+  language: "en-US"
+};
+
 const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  params: { api_key: "10923b261ba94d897ac6b81148314a3f", language: "en-US" }
+  baseURL: "https://api.themoviedb.org/3/"
 });
 
-export const movies = {
-  getMovie: id =>
-    api.get(`movie/${id}`, { params: { append_to_response: "videos" } }),
-  getPopular: () => api.get("movie/popular"),
-  getUpcoming: () => api.get("movie/upcoming"),
-  getNowPlaying: () => api.get("movie/now_playing"),
-  searchMovies: term =>
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing", { params }),
+  upcoming: () => api.get("movie/upcoming", { params }),
+  popular: () => api.get("movie/popular", { params }),
+  movieDetail: id =>
+    api.get(`movie/${id}`, {
+      params: { ...params, append_to_response: "videos" }
+    }),
+  search: term =>
     api.get("search/movie", {
-      params: {
-        query: encodeURIComponent(term)
-      }
+      params: { ...params, query: encodeURIComponent(term) }
     })
 };
 
-export const tv = {
-  getShow: id =>
-    api.get(`tv/${id}`, { params: { append_to_response: "videos" } }),
-  getPopular: () => api.get("tv/popular"),
-  getTopRated: () => api.get("tv/top_rated"),
-  getAiringToday: () => api.get("tv/airing_today"),
-  searchTv: term =>
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated", { params }),
+  popular: () => api.get("tv/popular", { params }),
+  airingToday: () => api.get("tv/airing_today", { params }),
+  showDetail: id =>
+    api.get(`tv/${id}`, {
+      params: { ...params, append_to_response: "videos" }
+    }),
+  search: term =>
     api.get("search/tv", {
-      params: {
-        query: encodeURIComponent(term)
-      }
+      params: { ...params, query: encodeURIComponent(term) }
     })
 };
